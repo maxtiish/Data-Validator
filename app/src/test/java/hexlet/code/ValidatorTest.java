eec1d7a9828915c994ef01135388a96ea8522f9d
maxtiish <cearness@mail.ru>
2023-11-21 18:04:39 +0700

    add tests

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,4 +1,41 @@
 package hexlet.code;
 
+import hexlet.code.schemas.StringSchema;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ValidatorTest {
+    private String testText1 = "Pumpkin pie";
+    private String testText2 = "Pumpkin pumpkin pie";
+    private final int minLength = 15;
+
+
+
+    @Test
+    public void testStringSchema() {
+        StringSchema obj = Validator.string();
+        Assertions.assertInstanceOf(StringSchema.class, obj);
+        assertTrue(obj.isValid(""));
+
+        obj.required();
+
+        assertFalse(obj.isValid(""));
+        assertFalse(obj.isValid(null));
+        assertTrue(obj.isValid(testText1));
+
+        obj.minLength(minLength);
+
+        assertTrue(obj.isValid(testText2));
+        assertFalse(obj.isValid(testText1));
+
+        obj.contains(testText2);
+
+        assertTrue(obj.isValid(testText2));
+        assertFalse(obj.isValid(testText1));
+
+
+    }
 }
