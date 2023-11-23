e5ce307dbe71ca45c9cb7c294105d0906cbeabe2
maxtiish <cearness@mail.ru>
2023-11-23 21:56:15 +0700

    update Validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
@@ -13,5 +14,9 @@ public class Validator {
 
     public static NumberSchema number() {
         return new NumberSchema();
+    }
+
+    public static MapSchema map() {
+        return new MapSchema();
     }
 }
